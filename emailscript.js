f320af9cfaf936ebffac8bf9b0028c72bd034020
Hiếu Tống <tonghieujp@gmail.com>
2023-09-28 16:17:08 +0900

    二回目 commit

--- emailscript.js
+++ emailscript.js
@@ -11,19 +11,27 @@
         // SMTPJSで登録したメールアドレス
         var smtpjsEmail = "kk.soft.info@gmail.com"; 
         //管理者のメールアドレス
-        var managerEmail = "faramining01@gmail.com"; 
+        var managerEmail = "faramining02@gmail.com"; 
 
  
 
   Email.send({
+    //トークン
     SecureToken: "192529-cb15-436b-81d8-ee6469e64e11",
+    //ホスト・サイト
     Host: "smtp.elasticemail.com",
+    //SMTPJSで登録したメールID
     Username: "kk.soft.info@gmail.com",
+    //SMTPJSで登録したメール・パスワード
     Password: "7591DA38AB3BC8070B4EEA9D39B78164E4E1",
-    From: smtpjsEmail, //SMTPJSで登録したメールアドレス
-    To: managerEmail, //管理者のメールアドレス
-    Subject: "K&Kソフト株式会社",
-    Body: "貴社名: " + companyName + "<br>" +
+    //SMTPJSで登録したメールアドレス
+    From: smtpjsEmail, 
+    //管理者のメールアドレス
+    To: managerEmail, 
+    Subject: "パートナー応募依頼「K&Kソフト株式会社」",
+    Body: "このたびはパートナー応募依頼において通知します。"+"<br>"+"<br>"+
+          "ご入力いただきました内容は以下の通りです。"+"<br>"+"<br>"+
+          "貴社名: " + companyName + "<br>" +
           "部署名: " + department + "<br>" +
           "ご担当者名: " + contactName + "<br>" +
           "メールアドレス: " + email + "<br>" +
@@ -31,9 +39,41 @@
           "備考: " + notes
   }).then(function(message) {
     if (message === "OK") {
-      alert("送信完了しました。");
+    //応募者まで確認メール
+      Email.send({
+        //トークン
+        SecureToken: "192529-cb15-436b-81d8-ee6469e64e11",
+        //ホスト・サイト
+        Host: "smtp.elasticemail.com",
+        //SMTPJSで登録したメールID
+        Username: "kk.soft.info@gmail.com",
+        //SMTPJSで登録したメール・パスワード
+        Password: "7591DA38AB3BC8070B4EEA9D39B78164E4E1",
+        //SMTPJSで登録したメールアドレス
+        From: smtpjsEmail, 
+        //応募者確認メール
+        To: email, 
+        Subject:  "[自動送信]パートナー応募へのお問い合わせありがとうございます。「K&Kソフト株式会社」",
+        Body: contactName+"様"+"<hr>"+"<br>"+"<br>"+
+        "このメールはシステムより自動返信にて送信させていただきます。"+"<br>"+"<br>"+
+        "このたびはパートナー応募におい問い合わせいただき、誠にありがとうございます。"+"<br>"+"<br>"+
+        "ご入力いただきました内容は以下の通りです。"+"<br>"+"<br>"+
+        "ご送信内容の確認"+"<br>"+"<br>"+
+        "貴社名: " + companyName + "<br>" +
+        "部署名: " + department + "<br>" +
+        "ご担当者名: " + contactName + "<br>" +
+        "メールアドレス: " + email + "<br>" +
+        "お電話番号: " + phoneNumber + "<br>" +
+        "備考: " + notes+"<br>"+
+        "<hr>"+
+        "ご不明な点やご質問がございましたら、ご気軽に下記までお問い合わせください"
+      }).then(function(message) {
+        if (message === "OK") {
+          alert("送信完全しました。");
+        }
+      });
     } else {
-      alert("発信エラー: " + message);
+      alert("エラー発生： " + message);
     }
       });
     };
